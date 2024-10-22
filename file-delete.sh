e9288c02fb40c846249c0572a1ba9aea89ef7ee7
Challa13 <59279517+Challa13@users.noreply.github.com>
2024-10-22 19:43:14 +0530

    first commit

---
+++ file-delete.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+
+while true; do
+    rm -rf /home/krishna/abcd.txt
+    sleep 10 # Sleep for 10 minutes
+done
---
+++ file-purge.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+
+while true; do
+    echo 0 > /home/krishna/abcd.txt
+    sleep 10 # Sleep for 10 minutes
+done
